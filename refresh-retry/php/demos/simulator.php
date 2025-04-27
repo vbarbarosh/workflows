e0c1744584d86179313e079c316fbf770e93bb44
Vladimir Barbarosh <vladimir.barbarosh@gmail.com>
2025-04-27 10:27:17 +0300

    simulator: progress

--- refresh-retry/php/demos/simulator.php
+++ refresh-retry/php/demos/simulator.php
@@ -15,10 +15,10 @@ function main(): void
     $now = Carbon::create('2020/01/01 00:00:00');
 
     info('👨‍💻 Manual Start');
-    foobar(REFRESH_RETRY_START, false);
+    handle_start_success_failure(REFRESH_RETRY_START, false);
 
     while (true) {
-        $next = array_filter([$db['refresh_at'], $jobs[0]['return_at'] ?? null]);
+        $next = array_filter([$db['refresh_at'], ...array_map(fn ($v) => $v['return_at'], $jobs)]);
         usort($next, function (Carbon $a, Carbon $b) {
             return $a->getTimestamp() <=> $b->getTimestamp();
         });
@@ -29,7 +29,6 @@ function main(): void
         $now = $next[0]->copy();
         process_jobs();
         process_poll();
-        $now->addHour();
         sleep(1);
     }
 }
@@ -47,17 +46,22 @@ function process_poll(): void
     if ($db['refresh_at'] === null || $now->lt($db['refresh_at'])) {
         return;
     }
-    foobar(REFRESH_RETRY_START);
+    handle_start_success_failure(REFRESH_RETRY_START);
 }
 
-function foobar(string $action, $log = true): void
+function handle_start_success_failure(string $action, $log = true): void
 {
     global $now, $db, $jobs;
 
     if ($log) {
         switch ($action) {
         case REFRESH_RETRY_START:
-            info('🚀 Refresh started');
+            if ($db['attempt_no'] === 0) {
+                info('🚀 Refresh started');
+            }
+            else {
+                info('🔄 Retry started');
+            }
             break;
         case REFRESH_RETRY_SUCCESS:
             info(sprintf('✅ Success (%d)', $db['attempt_no']));
@@ -71,16 +75,21 @@ function foobar(string $action, $log = true): void
     refresh_retry([
         'now' => $now,
         'rrule' => 'RRULE:FREQ=DAILY;BYHOUR=6,16;BYMINUTE=0;BYSECOND=0',
-        'retry_intervals' => ['PT0M', 'PT1M', 'PT5M', 'PT10M', 'PT15M'],
+        'retry_intervals' => ['PT0M', 'PT1M', 'PT5M'], // , 'PT10M', 'PT15M'],
         'timeout' => 'PT10M',
         'attempt_no' => $db['attempt_no'],
         'action' => $action,
         'fn' => function (RefreshAttempt $attempt) use (&$db) {
+            if ($attempt->final_failure) {
+                info('⚠️ No more retries. Wait until next planned refresh.');
+                $attempt->attempt_no = 0;
+                $attempt->refresh_at = $attempt->planned_refresh_at;
+            }
             $db['refresh_at'] = $attempt->refresh_at;
             $db['attempt_no'] = $attempt->attempt_no;
-            if ($attempt->final_failure) {
-                info('ℹ️ Refresh was disabled until the user reviewed it. An email about the incident was sent to the user.');
-            }
+//            if ($attempt->final_failure) {
+//                info('⚠️ Refresh was disabled until the user reviewed it. An email about the incident was sent to the user.');
+//            }
         },
     ]);
     if ($action !== REFRESH_RETRY_START) {
@@ -89,7 +98,7 @@ function foobar(string $action, $log = true): void
 
     $jobs[] = [
         'return_at' => $now->copy()->addMinutes(mt_rand(3, 5)),
-        'action' => mt_rand(0, 100) > 75 ? REFRESH_RETRY_SUCCESS : REFRESH_RETRY_FAILURE,
+        'action' => mt_rand(0, 100) > 60 ? REFRESH_RETRY_SUCCESS : REFRESH_RETRY_FAILURE,
     ];
 }
 
@@ -102,6 +111,6 @@ function process_jobs(): void
     }
 
     if ($now->gte($jobs[0]['return_at'])) {
-        foobar(array_shift($jobs)['action']);
+        handle_start_success_failure(array_shift($jobs)['action']);
     }
 }
